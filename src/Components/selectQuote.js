0adfff1b5f0e648389808dac60d4a5ca0a45fad5
Robert Frazier <robert.v.frazier@gmail.com>
2018-01-04 11:55:40 -0500

    Quotes appear on the screen.

--- src/Components/selectQuote.js
+++ src/Components/selectQuote.js
@@ -5,37 +5,44 @@ import {fetchQuotes} from '../Actions/quote';
 export class Quotes extends React.Component {
   componentDidMount(){
     this.props.dispatch(fetchQuotes());
-    this.getQuote();
+    // this.getQuote();
   }
 
-  getQuote() {
-    let quote;
-    let authorUsed;
-    fetch('http://localhost:8080/quotes')
-     .then(res => res.json())
-     .then(function(res) {
-       quote = res;
-       if(quote.author!==''){
-          authorUsed=quote.author;
-       } else{
-          authorUsed='Anonymous';
-       }
-       console.log(quote.quote+' — '+authorUsed);
-       return quote.quote;
-     });
-  }
+  // getQuote() {
+  //   let quote;
+  //   let authorUsed;
+  //   fetch('http://localhost:8080/quotes')
+  //    .then(res => res.json())
+  //    .then(function(res) {
+  //      quote = res;
+  //      if(quote.author!==''){
+  //         authorUsed=quote.author;
+  //      } else{
+  //         authorUsed='Anonymous';
+  //      }
+  //      console.log(quote.quote+' — '+authorUsed);
+  //      return quote.quote;
+  //    });
+  // }
 
   render(){
     return(
-      <div className="capture">
-        <form className="navBar">
-            {/* <img className="logo" alt="UnQuote logo" src="./images/UnQuote_logo.png"/> */}
-            <fieldset className="buttonBox">
-                <a href="pictures.html"><button type="button" className="button">New Meme</button></a>
-                <a href="meme.html"><button type="button" className="button">Rate Meme</button></a>
-            </fieldset>
-        </form>
-          <div>{this.props.fetchedQuote}</div>
+      <div className="container">
+        <div className="capture">
+          <form className="navBar">
+              {/* <img className="logo" alt="UnQuote logo" src="./images/UnQuote_logo.png"/> */}
+              <fieldset className="buttonBox">
+                  <a href="pictures.html"><button type="button" className="button">New Meme</button></a>
+                  <a href="meme.html"><button type="button" className="button">Rate Meme</button></a>
+              </fieldset>
+          </form>
+          <h1>
+              <div className="memeText">
+                <div className="quoteText">{this.props.quote.quote}</div> 
+                <div className="authorText">— {(this.props.quote.author !== '') ? this.props.quote.author : 'Anonymous'}</div>
+              </div>
+          </h1>
+        </div>
       </div>      
     )
   }
@@ -43,7 +50,7 @@ export class Quotes extends React.Component {
 
 const mapStateToProps = state => {
   return{
-  quotes: state.quotes,
+  quote: state.quote,
   loading: state.loading,
   error: state.error
 }};
