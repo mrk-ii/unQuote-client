87f349f20811899c89f6a982de90bc8f72809b11
Robert Frazier <robert.v.frazier@gmail.com>
2018-01-05 13:45:18 -0500

    Delete some commented-out code.

--- src/store.js
+++ src/store.js
@@ -1,12 +1,8 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from 'redux-thunk';
-// import { connect } from 'react-redux';
 import { fetchQuoteReducer } from './Reducers/quoteReducer';
 import { fetchImgReducer } from './Reducers/imgReducer';
-// import { fetchQuoteReducer as reducer } from 'redux';
-// import { fetchImgReducer as reducer } from 'redux';
-
 
 const rootReducer = combineReducers({quote: fetchQuoteReducer, img: fetchImgReducer});
 
